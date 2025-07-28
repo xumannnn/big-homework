c6e7caa3e2f03d6cdf8bf3f35c6c71e0e0fd1a33
xumannnn <funnymanxu@foxmail.com>
2025-07-28 19:52:18 +0800

    Delete movie-search-website directory

--- movie-search-website/movie_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class MovieAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movie_app'  # 应用的实际名称，和目录名对应
-    verbose_name = "网文管理"  # 这里设置在后台显示的应用名称
-
-# 然后在 movie_app 目录下的 __init__.py 文件中添加一行
-default_app_config = 'movie_app.MovieAppConfig'
--- movie-search-website/movie_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('search/', views.search, name='search'),
-    path('detail/<int:movie_id>/', views.detail, name='detail'),
-    path('category/<str:category>/', views.category, name='category'),
-]    
